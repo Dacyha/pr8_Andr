20a8398f53a4b25fe0a8d498a37c161389f218e1
colleg <colleg@cc.psu>
2022-12-08 18:43:16 +0300

    Изменили main.py на ветке dev

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import string
 def function(parol):
   lc='abcdefghijklmnopqrstuvwxyz'
   uc='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  num='1234567890'
   for i in range(11):
-    parol += parol.join(random.choice(lc+uc))
+    parol += parol.join(random.choice(lc+uc+num))
   return parol
 parol = ''
 print(function(parol))
