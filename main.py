8086d855b03bb1e5316402888c0be25435eb8f6d
colleg <colleg@cc.psu>
2022-12-08 18:35:23 +0300

    добавили в репозиторий main.py на ветке hotfix

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import random
 import string
 def function(parol):
   lc='abcdefghijklmnopqrstuvwxyz'
+  uc='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
   for i in range(11):
-    parol += parol.join(random.choice(lc))
+    parol += parol.join(random.choice(lc+uc))
   return parol
 parol = ''
 print(function(parol))
