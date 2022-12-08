4e2a740e8bb63e2aad4915d8dfabce0b7fbd2f4e
colleg <colleg@cc.psu>
2022-12-08 18:29:18 +0300

    изменили в репозитории первую версию main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
+import string
+def function(parol):
+  lc='abcdefghijklmnopqrstuvwxyz'
+  for i in range(11):
+    parol += parol.join(random.choice(lc))
+  return parol
+parol = ''
+print(function(parol))
 
